e9fb6d5d385a1c85d8348416d402cee3cb9be729
Gina Lucy <53538316+grlucy@users.noreply.github.com>
2019-11-26 18:15:30 -0500

    added tabs script

---
+++ assets/javascript/tabs.js
@@ -0,0 +1,18 @@
+// When user clicks the "Instructions" or "High Scores" tabs, the corresponding div becomes visible below
+
+let highScoresTab = document.getElementById("highScoresTab");
+let instructionsTab = document.getElementById("instructionsTab");
+
+highScoresTab.addEventListener("click", function(event) {
+  document.querySelector(".highScores").style.display = "block";
+  document.querySelector(".instructions").style.display = "none";
+  highScoresTab.style.borderBottom = "none";
+  instructionsTab.style.borderBottom = "1px solid #999";
+});
+
+instructionsTab.addEventListener("click", function(event) {
+  document.querySelector(".instructions").style.display = "block";
+  document.querySelector(".highScores").style.display = "none";
+  instructionsTab.style.borderBottom = "none";
+  highScoresTab.style.borderBottom = "1px solid #999";
+});
