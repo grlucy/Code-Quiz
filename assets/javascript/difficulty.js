4530f3f6590b2f40287aa0677051518ebbbfaaa4
Gina Lucy <53538316+grlucy@users.noreply.github.com>
2019-11-27 17:08:42 -0500

    added deactivated style to difficulty buttons

--- assets/javascript/difficulty.js
+++ assets/javascript/difficulty.js
@@ -1,20 +1,40 @@
-// When user clicks on "Easy" or "Hard" buttons, the button background-color changes and a boolean variable is assigned to store the active difficulty mode.
+// "Easy" and "Hard" buttons are only active when no game is in progress (timer says "000"). When user clicks on "Easy" or "Hard" buttons, the button background-color changes and a boolean variable is assigned to store the active difficulty mode.
 
 let easyBtn = document.getElementById("easyBtn");
 let hardBtn = document.getElementById("hardBtn");
 let easyMode = true;
 let hardMode = false;
 
-hardBtn.addEventListener("click", function(event) {
-  easyBtn.style.backgroundColor = "#999";
-  hardBtn.style.backgroundColor = "#444";
-  easyMode = false;
-  hardMode = true;
-});
+if (document.getElementById("time").textContent === "000") {
+  // Need to add style refresh for when game ends and buttons become active again - nested if statement - if easyMode, then this style; if hardMode, then this style
 
-easyBtn.addEventListener("click", function(event) {
-  hardBtn.style.backgroundColor = "#999";
-  easyBtn.style.backgroundColor = "#444";
-  easyMode = true;
-  hardMode = false;
-});
+  hardBtn.addEventListener("click", function(event) {
+    easyBtn.style.backgroundColor = "#999";
+    hardBtn.style.backgroundColor = "#444";
+    easyMode = false;
+    hardMode = true;
+  });
+
+  easyBtn.addEventListener("click", function(event) {
+    hardBtn.style.backgroundColor = "#999";
+    easyBtn.style.backgroundColor = "#444";
+    easyMode = true;
+    hardMode = false;
+  });
+} else if (easyMode) {
+  hardBtn.style.backgroundColor = "#aaa";
+  hardBtn.style.color = "#ccc";
+  hardBtn.style.cursor = "default";
+  easyBtn.style.backgroundColor = "#888";
+  easyBtn.style.color = "#ccc";
+  easyBtn.style.cursor = "default";
+  document.querySelector(".difficultyCaption").style.color = "#999";
+} else {
+  hardBtn.style.backgroundColor = "#888";
+  hardBtn.style.color = "#ccc";
+  hardBtn.style.cursor = "default";
+  easyBtn.style.backgroundColor = "#aaa";
+  easyBtn.style.color = "#ccc";
+  easyBtn.style.cursor = "default";
+  document.querySelector(".difficultyCaption").style.color = "#999";
+}
