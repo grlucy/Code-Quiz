7dbd488b2ca8ed729b749450e0b1cc4594484523
unknown <53538316+grlucy@users.noreply.github.com>
2019-11-29 22:12:07 -0500

    added easy and hard countdowns

--- assets/javascript/difficulty.js
+++ assets/javascript/difficulty.js
@@ -1,11 +1,12 @@
-// "Easy" and "Hard" buttons are only active when no game is in progress (timer says "000"). When user clicks on "Easy" or "Hard" buttons, the button background-color changes and a boolean variable is assigned to store the active difficulty mode.
+// "Easy" and "Hard" buttons are only active when no game is in progress (when startBtn is enabled). When user clicks on "Easy" or "Hard" buttons, the button background-color changes and a boolean variable is assigned to store the active difficulty mode.
 
 let easyBtn = document.getElementById("easyBtn");
 let hardBtn = document.getElementById("hardBtn");
 let easyMode = true;
 let hardMode = false;
+let startBtn = document.getElementById("startBtn");
 
-if (document.getElementById("time").textContent === "000") {
+if (startBtn.getAttribute("data-state") === "enabled") {
   // Need to add style refresh for when game ends and buttons become active again - nested if statement - if easyMode, then this style; if hardMode, then this style
 
   hardBtn.addEventListener("click", function(event) {
--- assets/javascript/start-quiz.js
+++ assets/javascript/start-quiz.js
@@ -1,13 +1,42 @@
 // When the user clicks "start quiz", the button becomes disabled, the timer starts counting down (from either 75 or 60, depending on difficulty setting), and the first quiz question appears.
 
-let startBtn = document.getElementById("startBtn");
+let easyCountdown;
+let hardCountdown;
 
+function easyTimer() {
+  setInterval(function() {
+    easyCountdown--;
+    document.getElementById("time").textContent = "0" + easyCountdown;
+  }, 1000);
+}
+function hardTimer() {
+  setInterval(function() {
+    hardCountdown--;
+    document.getElementById("time").textContent = "0" + hardCountdown;
+  }, 1000);
+}
+
+// User clicks start button
 startBtn.addEventListener("click", function(event) {
   if (startBtn.getAttribute("data-state") === "enabled") {
+    // Button is disabled
     startBtn.setAttribute("data-state", "disabled");
     startBtn.style.backgroundColor = "#bbb";
+    startBtn.style.cursor = "default";
     document.querySelector(".timeCaption").style.color = "#000";
     document.getElementById("time").style.color = "#000";
+
+    if (easyMode) {
+      // Timer is started with 75 seconds
+      easyCountdown = 75;
+      document.getElementById("time").textContent = "0" + easyCountdown;
+      easyTimer();
+    } else {
+      // Timer is started with 60 seconds
+      hardCountdown = 60;
+      document.getElementById("time").textContent = "0" + hardCountdown;
+      hardTimer();
+    }
   } else {
     return;
   }
@@ -16,6 +45,7 @@ startBtn.addEventListener("click", function(event) {
 function resetStartBtn() {
   startBtn.setAttribute("data-state", "enabled");
   startBtn.style.backgroundColor = "#444";
+  startBtn.style.cursor = "pointer";
   document.querySelector(".timeCaption").style.color = "#aaa";
   document.getElementById("time").style.color = "#aaa";
 }
