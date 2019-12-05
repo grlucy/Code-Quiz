cad0a2f9fc4f8eff84ee7fc8a3ff2c579dfbfec0
unknown <53538316+grlucy@users.noreply.github.com>
2019-12-05 12:48:32 -0500

    added media queries

--- assets/javascript/tabs.js
+++ assets/javascript/tabs.js
@@ -16,3 +16,18 @@ instructionsTab.addEventListener("click", function(event) {
   instructionsTab.style.borderBottom = "none";
   highScoresTab.style.borderBottom = "1px solid #999";
 });
+
+function hideDivsOnResize() {
+  if (window.innerWidth < 1024) {
+    // When screen size decreases to 1023px width or less, .highScores div and .instructions div are set to display:none
+    document.querySelector(".highScores").style.display = "none";
+    document.querySelector(".instructions").style.display = "none";
+  } else {
+    // When screen size increases to 1024px width or more, default to instructions tab/div visible
+    document.querySelector(".instructions").style.display = "block";
+    document.querySelector(".highScores").style.display = "none";
+    instructionsTab.style.borderBottom = "none";
+    highScoresTab.style.borderBottom = "1px solid #999";
+  }
+}
+window.onresize = hideDivsOnResize;
